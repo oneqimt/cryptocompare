54c51f5836ff09fa89fdbfc791fa4df67617f96a
oneqimt <oneqimt@gmail.com>
2018-03-14 15:33:34 -0700

    Init commit

--- src/main/java/com/imt11/crypto/util/SecurityUtil.java
+++ src/main/java/com/imt11/crypto/util/SecurityUtil.java
@@ -6,6 +6,11 @@ import javax.print.DocFlavor;
  * @author Dennis Miller
  */
 public class SecurityUtil {
+    public static final String HOST = "";
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
+    public static final String DRIVER = "";
+
 
     private SecurityUtil(){}
 
